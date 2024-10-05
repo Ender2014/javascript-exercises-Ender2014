cf3b1996e6e3d2b3afeec7a19e5ebe0287753823
Ender2014 <lau.zihao@gmail.com>
2024-10-05 19:02:51 +0800

    passed first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const strArray = string.split("");
+    let reversedStr = "";
 
+    for(let i=strArray.length-1; i>=0; i--){
+        reversedStr += strArray[i]
+    }
+
+    return reversedStr
 };
 
 // Do not edit below this line
