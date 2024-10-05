4891377b71548e8b618760fa148aecc556c7c848
Ender2014 <lau.zihao@gmail.com>
2024-10-05 20:38:59 +0800

    Passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
 
+
+const leapYears = function(year) {
+    const isNumDivisbleByFour = year % 4 === 0
+    const isNumDivisbleByCentury = year % 100 === 0
+    const isNumDivisbleBy400 = year % 400 === 0
+
+    if(isNumDivisbleByFour  && (!isNumDivisbleByCentury || isNumDivisbleBy400)){
+      return true
+    } else{
+        return false
+    }
 };
 
 // Do not edit below this line
