cc95805ee4054c3dd177a6d7c7210910e9e270c1
Ender2014 <lau.zihao@gmail.com>
2024-10-05 19:07:17 +0800

    Shortened code. Same function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,13 +1,5 @@
 const reverseString = function(string) {
-    const strArray = string.split("");
-    let reversedStr = "";
-
-    for(let i=strArray.length-1; i>=0; i--){
-        reversedStr += strArray[i]
-    }
-
-    return reversedStr
-};
-
+    return string.split("").reverse().join("")
+}
 // Do not edit below this line
 module.exports = reverseString;
