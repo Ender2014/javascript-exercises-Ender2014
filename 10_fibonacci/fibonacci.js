43a44d14927e19f60ac43cde8177b599f42e3b91
Ender2014 <lau.zihao@gmail.com>
2024-10-08 09:21:52 +0800

    Added solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let firstPrevNum = 0;
+    let secPrevNum = 1;
 
+    if(number < 0){
+        return "OOPS";
+    }
+
+    for(let i = 0; i < number; i++){
+        let current = firstPrevNum + secPrevNum;
+        firstPrevNum = secPrevNum;
+        secPrevNum = current;
+    }
+
+    return firstPrevNum;
 };
 
 // Do not edit below this line
