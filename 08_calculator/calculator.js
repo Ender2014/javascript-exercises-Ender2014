149b3f79fdd36e6fee2c5f5115e1a605e188b391
Ender2014 <lau.zihao@gmail.com>
2024-10-08 08:29:24 +0800

    Added solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, num) => product * num);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let finalNum = 1;
+	for(let i = 1; i < num + 1; i++){
+    finalNum *= i;
+  }
+  return finalNum;
 };
 
 // Do not edit below this line
