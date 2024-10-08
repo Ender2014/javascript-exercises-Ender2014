b7abe275292a89e7c43b4ac357ff422bd6db0ef1
Ender2014 <lau.zihao@gmail.com>
2024-10-08 08:44:06 +0800

    Added solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string="") {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const filteredStr = string
+        .toLowerCase()
+        .split("")   
+        .filter((char) => alphabet.includes(char))
+        .join("");
 
+    const reversedString = filteredStr.split("").reverse().join("");
+
+    return reversedString === filteredStr;
 };
 
 // Do not edit below this line
